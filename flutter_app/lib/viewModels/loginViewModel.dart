a2985e3571ea8c2b0943a12b9ff2f0f7f0c3ada1
dallalaste <gabrieldallelaste75@gmail.com>
2025-09-07 01:02:12 -0300

    fix: ligação com api

--- flutter_app/lib/viewModels/loginViewModel.dart
+++ flutter_app/lib/viewModels/loginViewModel.dart
@@ -1,11 +1,23 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<void> login(int id, String name, String email, String password) async{
+Future<void> login(int id, String name, String email, String password) async {
   final url = Uri.parse("https://atena-tb6q.onrender.com/cadastroUsuario");
-  final response = http.post(
-  url,
-  headers: {'Content-Type': 'application/json'},
-  body: jsonEncode({'id':id,'name':name, 'email': email, 'password': password})
+  final response = await http.post(
+    url,
+    headers: {'Content-Type': 'application/json'},
+    body: jsonEncode({
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+    }),
   );
+
+  if (response.statusCode == 201) {
+    print('Usuário cadastrado com sucesso');
+  } else {
+    print('Erro: ${response.statusCode}');
+    print('Mensagem: ${response.body}');
+  }
 }
