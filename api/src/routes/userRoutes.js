e110c71d9a460870fc1d2a6e14b48b443621e77f
dallalaste <gabrieldallelaste75@gmail.com>
2025-09-07 00:53:29 -0300

    fix: conexão com a api

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
 
-router.post('/cadastroUsuario', userController.cadastrarUsuario);
-router.get('/pegarUsuario', userController.pegarUsuario);
+router.post('/cadastroUsuario', UserController.cadastrarUsuario);
+router.get('/pegarUsuario', UserController.pegarUsuario);
 
 module.exports = router;
