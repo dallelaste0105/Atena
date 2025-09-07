de1080b0b5e1b0dfc55300ac21da7015f362fe73
dallalaste <gabrieldallelaste75@gmail.com>
2025-09-07 01:22:25 -0300

    fix: dados de conexao com o banco

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'maglev.proxy.rlwy.net',
-  port: 32168,
-  user: 'root',
-  password: 'XEksQYLHDQPaRckAhQLhwNjcbVUfGrSO',
-  database: 'railway'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
